a68b1f5b4e082f04c7a4553d812646e2a410c5cc
Saba Phruidze <sabaphruidze01@gmail.com>
2023-10-21 12:13:13 +0400

    start the project of nothing special but understanding core principes

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.box}>
+        <Text>Hello world</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +14,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  box: {
+    backgroundColor: "pink",
+    padding: 20,
   },
 });
